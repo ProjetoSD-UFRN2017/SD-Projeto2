b7b86ad1c63929779333407689e02af6680670f4
rianmartins <rianlopes@hotmail.com>
2017-05-15 01:14:06 -0300

    Envia valor do PWM e tempo em segundos

--- projeto2.py
+++ projeto2.py
@@ -1,15 +1,15 @@
 # Projeto Unidade 2
 # Alunos:
 #                Rian Lopes dos Santos Martins
-#		 Bruno Teonácio dos Santos
-#		 Natan de Lima Silva
-#		 Thárcito Pereira de Souza
+#                Bruno Teonácio dos Santos
+#                Natan de Lima Silva
+#                Thárcito Pereira de Souza
 # Disciplina: Sistemas Digitais
 # Prof: Marcelo
 
 
 from socket import *
-from threading import Thread
+import threading
 import time
 #import mraa
 
@@ -24,61 +24,87 @@ pino_led3 = 4 # Pino do led 3 #
 pino_pwm = 5 # Pino do pwm #
 
 # Outras variaveis #
+valor_pwm = 0
 delay_time = 0.05 # Tempo de delay #
 flag_chave = 0 # flag_chave == 1, sistema ligado | flag_chave == 0, sistema desligado #
 tempo_segundos = 0 # Tempo em segundos #
 valor_adc = 0 # Valor do conversor AD #
-
-# Contagem do tempo #
-def time_count():
-        global tempo_segundos
-	tempo_segundos = tempo_segundos + delay_time
-        time.sleep(delay_time)
-
-# Leitura do ADC #
-def ler_adc():
-        # global valor_adc = mraa.Aio(pino_sensor)
-        global valor_adc
-	valor_adc = 10
-
-# Geracao da curva #
-def curva_pwm(pwm,curva_selec,socket_client):
-
-	valor_pwm = 0
-        
-        if tempo_segundos >= 0 and tempo_segundos < 30: # 0 - 30 segundos #
-                #pwm.write(valor_adc)
-		valor_pwm = valor_adc*1 # Valor de teste
-
-        if tempo_segundos >= 30 and tempo_segundos < 60: # 30 - 60 segundos #
-                #pwm.write(valor_adc)
-		valor_pwm = valor_adc*2 # Valor de teste	
-
-        if tempo_segundos >= 60 and tempo_segundos < 90: # 60 - 90 segundos #
-                #pwm.write(valor_adc)
-		valor_pwm = valor_adc*3 # Valor de teste		
-
-        if tempo_segundos >= 90 and tempo_segundos < 120: # 90 - 120 segundos #
-                #pwm.write(valor_adc)
-		valor_pwm = valor_adc*4 # Valor de teste	
-
-        if tempo_segundos >= 120 and tempo_segundos < 180: # 120 - 180 segundos #
-                #pwm.write(valor_adc)
-		valor_pwm = valor_adc*5 # Valor de teste	
-
-	if(curva_selec == b'1\r\n'):
-		print('inserindo valor PWM da curva 1')
-		socket_client.sendall("0".encode())
-	else
-		print('inserindo valor PWM da curva 2')
-	
-	socket_client.sendall(str(valor_pwm).encode())
+curva = "" # Valor da curva
 
 # Configuracoes de conexao #
 serverName = 'localhost' # ip do servidor
 serverPort = 8015
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
+
+print('Iniciando o sistema, recebendo curva selecionada...')
+curva = clientSocket.recv(1024)
+print (curva)
+
+# Contagem do tempo #
+def time_count():
+        global tempo_segundos
+        tempo_segundos = tempo_segundos + delay_time
+        time.sleep(delay_time)
+
+# Leitura do ADC #
+def ler_adc():
+        # global valor_adc = mraa.Aio(pino_sensor)
+        global valor_adc
+        valor_adc = 10
+
+# Geracao da curva #
+def curva1_pwm():
+
+        global valor_pwm
+        valor_pwm = 0
+        
+        if tempo_segundos >= 0 and tempo_segundos < 30: # 0 - 30 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = (tempo_segundos*2.56)
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+
+        if tempo_segundos >= 30 and tempo_segundos < 60: # 30 - 60 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = 77
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+
+        if tempo_segundos >= 60 and tempo_segundos < 90: # 60 - 90 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = (tempo_segundos*3.8) - 151
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor            
+
+        if tempo_segundos >= 90 and tempo_segundos < 120: # 90 - 120 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = 191
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor      
+
+        if tempo_segundos >= 120 and tempo_segundos < 180: # 120 - 180 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = (tempo_segundos*(-3.18)) + 573
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor    
+
+def curva2_pwm():
+
+        global valor_pwm
+        valor_pwm = 0
+        
+        if tempo_segundos >= 0 and tempo_segundos < 30: # 0 - 30 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = (tempo_segundos*6.8)
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+
+        if tempo_segundos >= 30 and tempo_segundos < 150: # 30 - 60 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = 205
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+
+        if tempo_segundos >= 150 and tempo_segundos < 180: # 60 - 90 segundos #
+                #pwm.write(valor_adc)
+                valor_pwm = (tempo_segundos*(-6.8)) + 1230
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+
+
 
 # time.clock()
 
@@ -97,53 +123,66 @@ clientSocket.connect((serverName,serverPort))
 #mraa_pwm1.period_us(64)
 #mraa_pwm1.enable(True)
 
-print('Iniciando o sistema, recebendo curva selecionada...')
-curva = clientSocket.recv(1024)
-print (curva)
 
 ''' 
 # While Principal (com mraa)
 while True:
-	time_count()
-	if flag_chave == 1:
-		ler_adc()
-		curva_pwm(mraa_pwm1,curva,clientSocket)
-		if tempo_segundos > 180: # 3 minutos
-			tempo_segundos = 0
-			flag_chave = 0
+        time_count()
+        if flag_chave == 1:
+                ler_adc()
+                curva_pwm(mraa_pwm1,curva,clientSocket)
+                if tempo_segundos > 180: # 3 minutos
+                        tempo_segundos = 0
+                        flag_chave = 0
 
-	else:
-		chave = key.read()
-		if chave == 1:
-			tempo_segundos = 0
-			flag_chave = 1
+        else:
+                chave = key.read()
+                if chave == 1:
+                        tempo_segundos = 0
+                        flag_chave = 1
 '''
 
 # While de teste (sem Thread)
-while True:
-	time_count()
-	if tempo_segundos > 180: # 3 minutos
-		break
-	ler_adc()
-	curva_pwm(0,curva,clientSocket)
+# while True:
+#         time_count()
+#         if tempo_segundos > 180: # 3 minutos
+#                 break
+#         ler_adc()
+#         curva_pwm(0,curva,clientSocket)
 
 # While de teste (com Thread)
 #
-'''
+
 while True:
-	time_count()
-	if tempo_segundos > 180: # 3 minutos
-		break
+        time_count()
+        if tempo_segundos > 180: # 3 minutos
+                break
 
-	# Inicia as threads
-	t1 = threading.Thread(target=ler_adc)
-	t2 = threading.Thread(target=curva_pwm,args(0,curva,clientSocket,)) # Note o extra ',' (obrigatorio)
+        t1 = threading.Thread(target=ler_adc)
+        if(curva == b'1\r\n'):
+                #print('inserindo valor PWM da curva 1')
+                # Inicia as threads
+                t2 = threading.Thread(target=curva1_pwm)
+                print(valor_pwm)
+                clientSocket.send(str(valor_pwm).encode())
+                print(tempo_segundos)
+                clientSocket.send(str(tempo_segundos).encode())
+        else:
+                #print('inserindo valor PWM da curva 2')
+                # Inicia as threads
+                t2 = threading.Thread(target=curva2_pwm)
+                print(valor_pwm)
+                clientSocket.send(str(valor_pwm).encode())
+                print(tempo_segundos)
+                clientSocket.send(str(tempo_segundos).encode())
 
-	# Dispara as threads
-	t1.start()
-	t2.start()
-'''
+
+        # Dispara as threads
+        t1.start()
+        t2.start()
+
+
 #
-		
+                
 # Fim do sistema #
 clientSocket.close()
