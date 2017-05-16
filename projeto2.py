68f4c9c304c9958472a8ca93d39d8ff2e32601e0
rianmartins <rianlopes@hotmail.com>
2017-05-16 10:38:49 -0300

    atualização

--- projeto2.py
+++ projeto2.py
@@ -23,6 +23,8 @@ pino_led2 = 3 # Pino do led 2 #
 pino_led3 = 4 # Pino do led 3 #
 pino_pwm = 5 # Pino do pwm #
 
+#mraa_pwm_motor = 0 # declara pwm motor
+
 # Outras variaveis #
 valor_pwm = 0
 delay_time = 0.05 # Tempo de delay #
@@ -51,7 +53,8 @@ def time_count():
 def ler_adc():
         # global valor_adc = mraa.Aio(pino_sensor)
         global valor_adc
-        valor_adc = 10
+        x = mraa.Aio(pino_sensor)
+        valor_adc = x.read()
 
 # Geracao da curva #
 def curva1_pwm():
@@ -60,29 +63,29 @@ def curva1_pwm():
         valor_pwm = 0
         
         if tempo_segundos >= 0 and tempo_segundos < 30: # 0 - 30 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = (tempo_segundos*2.56)
+                valor_pwm = (tempo_segundos*0.01)
                 #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+                #mraa_pwm_motor.write(valor_pwm)
 
         if tempo_segundos >= 30 and tempo_segundos < 60: # 30 - 60 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = 77
+                valor_pwm = 0.3
                 #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+                #mraa_pwm_motor.write(valor_pwm)
 
         if tempo_segundos >= 60 and tempo_segundos < 90: # 60 - 90 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = (tempo_segundos*3.8) - 151
-                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor            
+                valor_pwm = (tempo_segundos*0.015) - 0.6
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+                #mraa_pwm_motor.write(valor_pwm)          
 
         if tempo_segundos >= 90 and tempo_segundos < 120: # 90 - 120 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = 191
-                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor      
+                valor_pwm = 0.75
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+                #mraa_pwm_motor.write(valor_pwm)     
 
         if tempo_segundos >= 120 and tempo_segundos < 180: # 120 - 180 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = (tempo_segundos*(-3.18)) + 573
-                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor    
+                valor_pwm = (tempo_segundos*(-0.0125)) + 2.25
+                #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
+                #mraa_pwm_motor.write(valor_pwm)
 
 def curva2_pwm():
 
@@ -90,18 +93,15 @@ def curva2_pwm():
         valor_pwm = 0
         
         if tempo_segundos >= 0 and tempo_segundos < 30: # 0 - 30 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = (tempo_segundos*6.8)
+                valor_pwm = (tempo_segundos*0.027)
                 #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
 
         if tempo_segundos >= 30 and tempo_segundos < 150: # 30 - 60 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = 205
+                valor_pwm = 0.8
                 #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
 
         if tempo_segundos >= 150 and tempo_segundos < 180: # 60 - 90 segundos #
-                #pwm.write(valor_adc)
-                valor_pwm = (tempo_segundos*(-6.8)) + 1230
+                valor_pwm = (tempo_segundos*(-0.027)) + 4.8
                 #valor_pwm = valor_pwm*valor_adc # Valor de atualizado pelo sensor
 
 
@@ -119,9 +119,10 @@ def curva2_pwm():
 #led2.dir(mraa.DIR_OUT)
 #led3.dir(mraa.DIR_OUT)
 
-#mraa_pwm1 = mraa.Pwm(pino_pwm)
-#mraa_pwm1.period_us(64)
-#mraa_pwm1.enable(True)
+#global mraa_pwm_motor
+#mraa_pwm_motor = mraa.Pwm(pino_pwm)
+#mraa_pwm_motor.period_us(64)
+#mraa_pwm_motor.enable(True)
 
 
 ''' 
@@ -164,6 +165,7 @@ while True:
                 # Inicia as threads
                 t2 = threading.Thread(target=curva1_pwm)
                 print(valor_pwm)
+                #mraa_pwm_motor.write(valor_pwm)
                 clientSocket.send(str(valor_pwm).encode())
                 print(tempo_segundos)
                 clientSocket.send(str(tempo_segundos).encode())
@@ -172,6 +174,7 @@ while True:
                 # Inicia as threads
                 t2 = threading.Thread(target=curva2_pwm)
                 print(valor_pwm)
+                #mraa_pwm_motor.write(valor_pwm)
                 clientSocket.send(str(valor_pwm).encode())
                 print(tempo_segundos)
                 clientSocket.send(str(tempo_segundos).encode())
